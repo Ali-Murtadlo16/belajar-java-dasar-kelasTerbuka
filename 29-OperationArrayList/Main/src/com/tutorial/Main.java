fc890a24880cf1180f5cdbabd5d62413ae1b84cb
Alimurtadlo16 <ahmadalimurtadloasadillah@gmail.com>
2026-06-23 11:00:37 +0700

    Operation in Array List

--- 29-OperationArrayList/Main/src/com/tutorial/Main.java
+++ 29-OperationArrayList/Main/src/com/tutorial/Main.java
@@ -3,9 +3,14 @@ public class Main{
     public static void main(String[] args) {
         Hero hero1 = new Hero("Ali", 100);
         Hero hero2 = new Hero("Murtadlo", 100);
+        IntelHero hero3 = new IntelHero("Sri", 100);
+        AgilityHero hero4 = new AgilityHero("Dewi", 100);
 
         hero1.display();
         hero2.display();
         System.out.println();
+
+        hero3.display();
+        hero4.display();
     }
 }
