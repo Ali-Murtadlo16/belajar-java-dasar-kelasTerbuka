440d9a6fe42e8b45a39ff67320f94060915053b2
Ali-Murtadlo16 <ahmadalimurtadloasadillah@gmail.com>
2026-09-22 08:57:00 +0700

    References In Object in Java

--- 05-ReferencesInObject/Main/src/com/tutorial/Main.java
+++ 05-ReferencesInObject/Main/src/com/tutorial/Main.java
@@ -3,28 +3,31 @@ class Buku {
     String judul;
     String penulis;
     String penerbit;
+    int tahunTerbit;
 
-    Buku(String judul, String penulis, String penerbit) {
+    Buku(String judul, String penulis, String penerbit, int tahunTerbit) {
         this.judul = judul;
         this.penulis = penulis;
         this.penerbit = penerbit;
+        this.tahunTerbit = tahunTerbit;
     }
 
     void display(){
         System.out.println("Judul: " + judul);
         System.out.println("Penulis: " + penulis);
         System.out.println("Penerbit: " + penerbit);
+        System.out.println("Tahun Terbit: " + tahunTerbit);
     }
 }
 public class Main {
     public static void main(String[] args) {
-        Buku dataBuku1 = new Buku("Pemrograman Java", "Ali Murtadlo", "Penerbit ABC");
+        Buku dataBuku1 = new Buku("Pemrograman Java", "Ali Murtadlo", "Penerbit ABC", 2021);
         dataBuku1.display();
 
         // Show address of dataBuku1
         String addressBuku1 =Integer.toHexString(System.identityHashCode(dataBuku1));
         System.out.println(addressBuku1);
-    
+
         // Assignment object
         Buku dataBuku2 = dataBuku1;
         dataBuku2.display();
@@ -35,10 +38,10 @@ public class Main {
         dataBuku1.judul = "Pemrograman Java Lanjutan";
         dataBuku1.display();
         dataBuku2.display();
-    
+
         //Insert object methods
         fungsi(dataBuku1);
-    
+
     }
 
     public static void fungsi(Buku dataBuku){
