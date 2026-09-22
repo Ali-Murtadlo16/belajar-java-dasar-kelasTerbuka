48e4d6d69306f2fe0e8f6f371e84dea5e6860376
Ali-Murtadlo16 <ahmadalimurtadloasadillah@gmail.com>
2026-09-22 09:42:16 +0700

    Basic Attacking on java use Player

---
+++ 09-BasicAttackingTrial/Main/src/com/tutorial/Main.java
@@ -0,0 +1,189 @@
+package com.tutorial;
+
+class Player {
+    private final String name;
+    private final int baseHealth;
+    private final int baseAttack;
+    private Armor armor;
+    private Weapon weapon;
+    private int level;
+    private final int incrementAttack;
+    private int currentHealth;
+
+    public Player(String name){
+        this.name = name;
+        this.baseHealth = 1000;
+        this.baseAttack = 30;
+        this.level = 1;
+        this.incrementAttack = 10;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getCurrentHealth() {
+        return this.currentHealth;
+    }
+
+    public boolean isAlive() {
+        return this.currentHealth > 0;
+    }
+
+    public void levelUp(){
+        this.level++;
+        this.currentHealth = this.maxHealth();
+    }
+
+    public void initHealth() {
+        this.currentHealth = this.maxHealth();
+    }
+
+    public void display(){
+        System.out.println("Player\t\t: " + this.name);
+        System.out.println("Level\t\t: " + this.level);
+        System.out.println("Health\t\t: " + this.currentHealth + "/" + this.maxHealth());
+        System.out.println("Attack\t\t: " + this.getAttack());
+        System.out.println("---------------------------------");
+    }
+
+    public void setArmor(Armor armor){
+        this.armor = armor;
+        initHealth();
+    }
+
+    public void setWeapon(Weapon weapon){
+        this.weapon = weapon;
+    }
+
+    public int maxHealth(){
+        if (this.armor != null) {
+            return this.baseHealth + this.armor.getAddHealth();
+        }
+        return this.baseHealth;
+    }
+
+    public int getAttack(){
+        return this.getAttackPower();
+    }
+
+    public int getAttackPower(){
+        int weaponAttack = 25;
+        if (this.weapon != null) {
+            weaponAttack = this.weapon.getAttack();
+        }
+        return this.baseAttack + this.level * this.incrementAttack + weaponAttack;
+    }
+
+    public void attack(Player enemy) {
+        int damageDealt = this.getAttack();
+        System.out.println("⚔️ [" + this.name + "] menyerang [" + enemy.getName() + "] sebesar " + damageDealt + " DAMAGE!");
+        enemy.takeDamage(damageDealt);
+    }
+
+    public void takeDamage(int damage) {
+        int defense = 50;
+        if (this.armor != null) {
+            defense = this.armor.getStrength() * 5;
+        }
+
+        int netDamage = damage - defense;
+        if (netDamage < 1) netDamage = 1;
+
+        this.currentHealth -= netDamage;
+        if (this.currentHealth < 0) this.currentHealth = 0;
+
+        System.out.println("🛡️ [" + this.name + "] menahan serangan (Defense: " + defense + "). Sisa HP: " + this.currentHealth);
+    }
+}
+
+class Weapon {
+    private final String name;
+    private final int attack;
+
+    public Weapon(String name, int attack){
+        this.name = name;
+        this.attack = attack;
+    }
+
+    public int getAttack(){
+        return this.attack;
+    }
+}
+
+class Armor {
+    private final String name;
+    private final int strength;
+    private final int health;
+
+    public Armor(String name, int strength, int health){
+        this.name = name;
+        this.strength = strength;
+        this.health = health;
+    }
+
+    public int getAddHealth(){
+        return this.strength * 10 + this.health;
+    }
+
+    public int getStrength(){
+        return this.strength;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        Player player1 = new Player("Ali");
+        Armor armor1 = new Armor("Baju Besi", 10, 150);
+        Weapon weapon1 = new Weapon("Pedang", 45);
+        player1.setArmor(armor1);
+        player1.setWeapon(weapon1);
+
+        Player player2 = new Player("Murtadlo");
+        Armor armor2 = new Armor("Baju Baja", 25, 70);
+        Weapon weapon2 = new Weapon("Keris", 35);
+        player2.setArmor(armor2);
+        player2.setWeapon(weapon2);
+
+        System.out.println("=== KONDISI AWAL PETARUNG ===");
+        player1.display();
+        player2.display();
+
+        System.out.println("\n🔥 PERTANDINGAN DIMULAI 🔥\n");
+        int ronde = 1;
+
+        while (player1.isAlive() && player2.isAlive()) {
+            System.out.println("--- RONDE " + ronde + " ---");
+
+            player1.attack(player2);
+            System.out.println();
+
+            Thread.sleep(1000);
+
+            if (!player2.isAlive()) {
+                break;
+            }
+
+            player2.attack(player1);
+            System.out.println();
+
+            Thread.sleep(1000);
+
+            ronde++;
+        }
+
+        System.out.println("=================================");
+        System.out.println("💥 PERTANDINGAN BERAKHIR! 💥");
+        System.out.println("=================================");
+
+        if (player1.isAlive()) {
+            System.out.println("🏆 PEMENANGNYA ADALAH: [" + player1.getName() + "] 🎉");
+        } else {
+            System.out.println("🏆 PEMENANGNYA ADALAH: [" + player2.getName() + "] 🎉");
+        }
+
+        System.out.println("\n=== STATUS AKHIR ===");
+        player1.display();
+        player2.display();
+    }
+}
