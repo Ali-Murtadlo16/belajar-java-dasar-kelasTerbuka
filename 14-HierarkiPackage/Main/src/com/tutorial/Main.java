0fb70e08afcbfbb4c28384957211cb2bf6c38f1a
Ali-Murtadlo16 <ahmadalimurtadloasadillah@gmail.com>
2026-09-24 09:39:33 +0700

    Collab Other Package in One Source of Code in Java

--- 14-HierarkiPackage/Main/src/com/tutorial/Main.java
+++ 14-HierarkiPackage/Main/src/com/tutorial/Main.java
@@ -5,13 +5,25 @@ import static com.terminal.Console.log;
 // Can't be private
 class Main{
     public static void main(String[] args){
-        Player player1 = new Player("Ali");
+        Player player1 = new Player("Ahmad");
+        Player player2 = new Player("Ali");
+        Player player3 = new Player("Murtadlo");
+        Player player4 = new Player("Asadillah");
         player1.show();
+        player2.show();
+        player3.show();
+        player4.show();
 
         Console.log("\nMenampilkan data dengan console log");
         Console.log(player1.getName());
-        
+        Console.log(player2.getName());
+        Console.log(player3.getName());
+        Console.log(player4.getName());
+
         log("\nMenampilkan data dengan log saja");
         log(player1.getName());
+        log(player2.getName());
+        log(player3.getName());
+        log(player4.getName());
     }
 }
