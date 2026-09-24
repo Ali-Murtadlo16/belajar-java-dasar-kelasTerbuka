664d229279d7511ec4b7098d475e1b5df78ef354
Ali-Murtadlo16 <ahmadalimurtadloasadillah@gmail.com>
2026-09-24 09:34:42 +0700

    Use Import From Other Packages

--- 13-MultiFileWithPackage/Main/src/com/tutorial/Main.java
+++ 13-MultiFileWithPackage/Main/src/com/tutorial/Main.java
@@ -1,14 +1,17 @@
 package com.tutorial;
-// Keyword import package extern
 import com.terminal.Console;
 class Main {
     public static void main(String[] args){
-        Player player1 = new Player("Ali");
-        Player player2 = new Player("Murtadlo");
+        Player player1 = new Player("Ahmad", "Bandung");
+        Player player2 = new Player("Ali", "Jakarta");
+        Player player3 = new Player("Murtadlo", "Surabaya");
+        Player player4 = new Player("Asadillah", "Bandung");
         player1.show();
         player2.show();
+        player3.show();
+        player4.show();
 
         Console.log("Hello ");
         Console.log("World ");
-    }    
+    }
 }
--- 13-MultiFileWithPackage/Main/src/com/tutorial/Player.java
+++ 13-MultiFileWithPackage/Main/src/com/tutorial/Player.java
@@ -1,12 +1,17 @@
 package com.tutorial;
 
+import java.awt.PageAttributes;
+
 class Player{
     private String name;
-    Player(String name){
+    private String daerah;
+    Player(String name, String daerah){
         this.name = name;
+        this.daerah = daerah;
     }
 
     void show(){
         System.out.println("Player name : " + this.name);
+        System.out.println("Player daerah : " + this.daerah);
     }
 }
